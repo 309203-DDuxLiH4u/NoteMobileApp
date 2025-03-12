a3ad6db297e11f716ecbdc4a2620fbfe94e2b568
unknown <peotoxic3009@gmail.com>
2025-03-12 17:28:18 +0700

    Updated SingUp Screen and CustomRextField

--- app/src/main/java/com/example/shopping/presentation/SignUpScreen.kt
+++ app/src/main/java/com/example/shopping/presentation/SignUpScreen.kt
@@ -2,7 +2,10 @@ package com.example.shopping.presentation
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -17,6 +20,8 @@ import androidx.compose.material3.Text
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.shopping.presentation.Utils.CustomTextField
+import com.example.shopping.presentation.Utils.CustomTextField
 
 @Preview(showSystemUi = true)
 @Composable
@@ -45,5 +50,14 @@ fun SignUpScreen() {
             style = TextStyle(fontWeight = FontWeight.Bold),
             modifier = Modifier.padding(vertical = 16.dp).align(Alignment.Start)
         )
+
+        CustomTextField(
+            value = firstName,
+            onValueChange = {firstName = it},
+            label = "First Name",
+            leadingIcon = Icons.Default.Person,
+            modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp),
+        )
+
     }
 }
