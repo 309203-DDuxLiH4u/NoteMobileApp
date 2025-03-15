186d7cb156b3da9eaa500e9f90022af4658667e0
unknown <peotoxic3009@gmail.com>
2025-03-16 02:36:58 +0700

    completed LoginUI and SignUpUI

--- app/src/main/java/com/example/shopping/presentation/LoginScreen.kt
+++ app/src/main/java/com/example/shopping/presentation/LoginScreen.kt
@@ -1,7 +1,9 @@
 package com.example.shopping.presentation
 
 import android.widget.Toast
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -9,16 +11,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Lock
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Button
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -27,60 +25,44 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.shopping.R
 import com.example.shopping.presentation.Utils.CustomTextField
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.TextButton
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 
-@Preview(showSystemUi = true)
 @Composable
-fun SignUpScreen() {
+fun LoginScreen() {
 
     val context = LocalContext.current
-    var firstName by remember { mutableStateOf("") }
-    var lastName by remember { mutableStateOf("") }
-    var password by remember { mutableStateOf("") }
-    var confirmPassword by remember { mutableStateOf("") }
-    var phoneNumber by remember { mutableStateOf("") }
+
     var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
 
-
-
-    Column( modifier = Modifier.fillMaxSize(),
+    Column(
+        modifier = Modifier.fillMaxSize()
+            .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
 
-
-
-        ) {
-
+    )
+    {
         Text(
-            text = "SignUp",
+            text = "Login",
             fontSize = 24.sp,
             style = TextStyle(fontWeight = FontWeight.Bold),
-            modifier = Modifier.padding(vertical = 16.dp).align(Alignment.Start)
-        )
-
-        CustomTextField(
-            value = firstName,
-            onValueChange = { firstName = it },
-            label = "First Name",
-            leadingIcon = Icons.Default.Person,
-            modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp),
-        )
-        CustomTextField(
-            value = lastName,
-            onValueChange = { lastName = it },
-            label = "Last Name",
-            leadingIcon = Icons.Default.Person,
-            modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp),
+            modifier = Modifier
+                .padding(vertical = 16.dp)
+                .align(Alignment.Start)
         )
         CustomTextField(
             value = email,
@@ -89,58 +71,48 @@ fun SignUpScreen() {
             leadingIcon = Icons.Default.Email,
             modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp),
         )
+        Spacer(modifier = Modifier.padding(8.dp))
         CustomTextField(
             value = password,
             onValueChange = { password = it },
-            label = "Create Password",
-            leadingIcon = Icons.Default.Lock,
-            modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp),
+            label = "Password",
             visualTransformation = PasswordVisualTransformation(),
-        )
-        CustomTextField(
-            value = confirmPassword,
-            onValueChange = { confirmPassword = it },
-            label = "Confirm Password",
             leadingIcon = Icons.Default.Lock,
-            modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp),
-            visualTransformation = PasswordVisualTransformation(),
+            modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp),
+
+            )
+        Spacer(modifier = Modifier.padding(4.dp))
+
+        Text(
+            text = "Forgot Password?",
+            modifier = Modifier.align(Alignment.End)
         )
-        CustomTextField(
-            value = phoneNumber,
-            onValueChange = { phoneNumber = it },
-            label = "Phone",
-            leadingIcon = Icons.Default.Phone,
-            modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp),
-        )
+
+        Spacer(modifier = Modifier.padding(16.dp))
+
         Button(
             onClick = {
 
+                if (email.isNotBlank() && password.isNotBlank()) {
 
-                if (firstName.isNotEmpty() && lastName.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty() && phoneNumber.isNotEmpty()) {
-
-
-                    if (password == confirmPassword) {
-
-                        Toast.makeText(context, "Sign Up Successful", Toast.LENGTH_SHORT).show()
-                    } else {
-                        Toast.makeText(
-                            context,
-                            "Password and Confirm password not match",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
+                    Toast.makeText(context, "Login Successful", Toast.LENGTH_SHORT).show()
                 } else {
                     Toast.makeText(context, "Please fill all details", Toast.LENGTH_SHORT).show()
                 }
+
             },
-            modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 16.dp),
             shape = RoundedCornerShape(8.dp),
-            colors = ButtonDefaults.buttonColors(
-                colorResource(id = R.color.orange)
-            )
-        ){
-            Text("Sign Up", color = colorResource(id = R.color.white))
+            colors = ButtonDefaults.buttonColors(colorResource(id = R.color.orange)),
+            border = BorderStroke(1.dp, colorResource(id = R.color.orange))
+
+        ) {
+            Text("Login", color = Color.White)
+
         }
+
         Row(verticalAlignment = Alignment.CenterVertically){
 
             Text("Already have an account?")
@@ -153,7 +125,7 @@ fun SignUpScreen() {
         Row(Modifier.fillMaxWidth().padding(vertical = 8.dp),
 
             verticalAlignment = Alignment.CenterVertically
-            ){
+        ){
 
             HorizontalDivider(modifier = Modifier.weight(1f))
 
@@ -173,5 +145,6 @@ fun SignUpScreen() {
             Spacer(modifier = Modifier.size(8.dp))
             Text("Login with Google", color = colorResource(id = R.color.orange))
         }
+
     }
 }
